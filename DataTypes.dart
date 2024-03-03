3a589c6463d1705591d5aeed6724f8455503e8e5
Glory Lazaro Msasalaga <131989209+GloryMsasalaga@users.noreply.github.com>
2024-03-03 18:06:50 +0300

    Add DataTypes.dart

---
+++ DataTypes.dart
@@ -0,0 +1,27 @@
+void main() {
+  //Integers: used to represent whole numbers
+  int age = 25;
+
+  //Doubles: used to represent floating-point numbers
+  double weight = 75.5;
+
+  //Strings: used to represent textual data
+  String name = 'Glory Lazaro';
+
+  //List: used to represent an ordered collection of objects
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  //Maps: used to represent a collection of key-value pairs
+  Map<String, String> person = {
+    'name': 'Glory',
+    'age': '23',
+    'job': 'Geomatician and Computer Networker'
+  };
+
+  //Printing out the values to demonstrate their use
+  print('Age: $age');
+  print('Weight: $weight');
+  print('Name: $name');
+  print('Numbers: $numbers');
+  print('Person: $person');
+}
